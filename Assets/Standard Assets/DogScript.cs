49ed580829dfec486d1e442bbce71adeeb076112
Rose Flynn <cinorflynn@yahoo.com>
2019-09-16 10:59:15 -0400

    Upload
    Upload with no synty assets

---
+++ Assets/Standard Assets/DogScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class DogScript : MonoBehaviour
+{
+
+    public static DogScript dog = null;
+    NavMeshAgent myAgent;
+    public GameObject targetObj;
+    private void Awake()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        if(dog == null)
+        {
+            dog = this;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myAgent.SetDestination(targetObj.transform.position);
+    }
+
+    public void SetBallTarget(GameObject ball)
+    {
+        targetObj = ball;
+       
+    }
+}
